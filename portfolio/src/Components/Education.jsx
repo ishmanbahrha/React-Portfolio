8b39f8d981fe7e98a6c617c9c7fa48decb9cfdc2
Ishman <ishman.bahrha@gmail.com>
2024-06-04 00:08:27 +0530

    Editing carousel image & skill cards

--- portfolio/src/Components/Education.jsx
+++ portfolio/src/Components/Education.jsx
@@ -2,7 +2,6 @@ import Carousel from 'react-bootstrap/Carousel';
 import Paint from '../assets/Paint.png';
 import Paint2 from '../assets/Paint2.png';
 
-
 function CarouselFadeExample() {
   return (
     <div className = "container" style = {{marginTop:"45px",display:"flex"}}>
@@ -12,16 +11,15 @@ function CarouselFadeExample() {
 
     <Carousel fade className = "eduCards" style = {{margin:"auto"}}>
       <Carousel.Item  className = "eduCards">
-        <img src = {Paint2} alt="First slide"/>
+        <img className = "imageFade" src = {Paint} alt="First slide"/>
         <Carousel.Caption style = {{color:"black"}}>
           <h3>Bachelor of Technology | B.Tech</h3>
           <p>2017-2021</p>
         </Carousel.Caption>
       </Carousel.Item>
 
-
       <Carousel.Item  className = "eduCards">
-        <img src = {Paint2} alt="Second slide"/>
+        <img className = "imageFade" src = {Paint} alt="Second slide"/>
         <Carousel.Caption style = {{color:"black"}}>
           <h3>Electronics and Communication Engineering</h3>
           <p>CGPA - 9.02</p>
